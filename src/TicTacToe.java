10b38a59f4ee08d30f9d1ee35e4e5b8ddc119e56
FREDERIK DIAMOND <unknownuser0709@proton.me>
2023-10-02 13:26:16 +0200

    General styling of UI added

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class TicTacToe implements ActionListener {
     Random random = new Random();
@@ -12,10 +13,37 @@ public class TicTacToe implements ActionListener {
     JButton[] buttons = new JButton[9];
     boolean player1_turn;
 
-    void TacTacToe() {
+    TicTacToe() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
         frame.getContentPane().setBackground(new Color(50, 50, 50));
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        textField.setBackground(new Color(25, 25, 25));
+        textField.setForeground(new Color(25, 255, 0));
+        textField.setFont(new Font("Ink Free", Font.BOLD, 75));
+        textField.setHorizontalAlignment(JLabel.CENTER);
+        textField.setText("Tic-Tac-Toe");
+        textField.setOpaque(true);
+
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0, 0, 800, 100);
+
+        button_panel.setLayout(new GridLayout(3, 3));
+        button_panel.setBackground(new Color(150, 150, 150));
+
+        for (int i = 0; i < 9; i++) {
+            buttons[i] = new JButton();
+            button_panel.add(buttons[i]);
+            buttons[i].setFont(new Font("MV Boli", Font.BOLD, 120));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this);
+        }
+
+        title_panel.add(textField);
+        frame.add(title_panel, BorderLayout.NORTH);
+        frame.add(button_panel);
     }
 
     @Override
